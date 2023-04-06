9939fb423181d2aeedae5986e98573a5cb1fec61
Hamed Jimoh <hameed.jimoh1601@gmail.com>
2023-04-06 05:22:43 +0100

    Created type for all notifications

--- src/services/notifications/NotificationTypes.ts
+++ src/services/notifications/NotificationTypes.ts
@@ -1,11 +1,48 @@
-type OrderNotificationReturnedPayload = {
+interface NotificationDataTypes {
   _id: string;
-  orderId: string;
   isRead: boolean;
   createdAt: string;
   updatedAt: string;
+}
+
+export interface OrderNotificationReturnedPayload
+  extends NotificationDataTypes {
+  orderId: string;
+}
+
+export interface CustomerNotificationReturnedPayload
+  extends NotificationDataTypes {
+  customerId: string;
+}
+
+export interface LowStockNotificationReturnedPayload
+  extends NotificationDataTypes {
+  productId: string;
+  productName: string;
+  currentStockQuantity: string;
+  brandName: string;
+}
+
+export interface PreOrderNotificationReturnedPayload
+  extends NotificationDataTypes {
+  productId: string;
+  productName: string;
+  customerName: string;
+  brandName: string;
+}
+
+export type OrderNotificationPayloadType = {
+  data: OrderNotificationReturnedPayload[];
 };
 
-export type OrderNotificationType = {
-  data: OrderNotificationReturnedPayload[];
+export type CustomerNotificationPayloadType = {
+  data: CustomerNotificationReturnedPayload[];
+};
+
+export type LowStockNotificationPayloadType = {
+  data: LowStockNotificationReturnedPayload[];
+};
+
+export type PreOrderNotificationPayloadType = {
+  data: PreOrderNotificationReturnedPayload[];
 };
