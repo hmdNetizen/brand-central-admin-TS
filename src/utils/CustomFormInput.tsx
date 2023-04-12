1daa14338e7806251467a54c15a7cd4b74d26471
Hamed Jimoh <hameed.jimoh1601@gmail.com>
2023-04-12 11:04:24 +0100

    Creates CustomFormInput component

---
+++ src/utils/CustomFormInput.tsx
@@ -0,0 +1,33 @@
+import React, { Fragment } from "react";
+import { FormInput, FormLabel, Error } from "./styles/CustomFormInputStyles";
+
+type FormInputProps = {
+  labelId: string;
+  label: string;
+  type: string;
+  placeholder: string;
+  error: string;
+  labelColor: string;
+};
+
+const CustomFormInput = (props: FormInputProps) => {
+  const { labelId, label, type, placeholder, error, labelColor, ...rest } =
+    props;
+  return (
+    <Fragment>
+      <FormLabel htmlFor={labelId} labelColor={labelColor}>
+        {label}
+      </FormLabel>
+      <FormInput
+        type={type}
+        id={labelId}
+        placeholder={placeholder}
+        autoComplete="off"
+        {...rest}
+      />
+      {error && <Error>{error}</Error>}
+    </Fragment>
+  );
+};
+
+export default CustomFormInput;
---
+++ src/utils/styles/CustomFormInputStyles.tsx
@@ -0,0 +1,45 @@
+import { styled } from "@mui/material/styles";
+
+type FormLabelProps = {
+  labelColor: string;
+};
+
+export const FormLabel = styled("label", {
+  shouldForwardProp: (prop) => prop !== "labelColor",
+})<FormLabelProps>(({ theme, labelColor }) => ({
+  display: "block",
+  fontSize: "1.5rem",
+  color: labelColor ? labelColor : theme.palette.secondary.main,
+  fontFamily: "Open Sans, Roboto",
+  fontWeight: 500,
+  marginBottom: ".75rem",
+}));
+
+export const FormInput = styled("input")(({ theme }) => ({
+  display: "block",
+  width: "100%",
+  fontSize: "1.6rem",
+  fontWeight: 500,
+  padding: "1rem 2rem",
+  border: `1px solid ${theme.palette.common.lightGrey}`,
+  borderRadius: 5,
+  color: theme.palette.primary.light,
+  height: 43,
+
+  "&:focus": {
+    outline: "none",
+  },
+
+  "&::placeholder": {
+    fontWeight: 500,
+    fontSize: "1.4rem",
+    color: "#ccc",
+  },
+}));
+
+export const Error = styled("small")(({ theme }) => ({
+  fontSize: "1.2rem",
+  color: theme.palette.error.main,
+  fontFamily: "Open Sans, Roboto",
+  fontWeight: 500,
+}));
