2442346e077a1d8a3c2f33f1cdf9e9581deef05e
Hamed Jimoh <hameed.jimoh1601@gmail.com>
2023-04-14 12:47:10 +0100

    Implements deletePreorder api call

--- src/services/pre-orders/PreOrderTypes.ts
+++ src/services/pre-orders/PreOrderTypes.ts
@@ -1,11 +1,19 @@
+import React from "react";
 import { ProductTypes } from "../products/ProductTypes";
 
 export type PreOrderedPayloadType = {
   data: ProductTypes[];
 };
 
+export type DeletePreOrderType = {
+  preOrderId: string;
+  setOpenDeletePreOrder: React.Dispatch<React.SetStateAction<boolean>>;
+};
+
 export type initStateType = {
   loadingPreOrder: boolean;
+  loadingPreOrderAction: boolean;
   preOrders: ProductTypes[];
+  singlePreOrder: null | ProductTypes;
   error: null | string;
 };
--- src/services/pre-orders/index.ts
+++ src/services/pre-orders/index.ts
@@ -1,10 +1,18 @@
-import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk, PayloadAction } from "@reduxjs/toolkit";
 import axios from "../axios";
-import { initStateType, PreOrderedPayloadType } from "./PreOrderTypes";
+import { toast } from "react-toastify";
+import {
+  initStateType,
+  PreOrderedPayloadType,
+  DeletePreOrderType,
+} from "./PreOrderTypes";
+import { ProductTypes } from "../products/ProductTypes";
 
 const initialState: initStateType = {
   loadingPreOrder: false,
+  loadingPreOrderAction: false,
   preOrders: [],
+  singlePreOrder: null,
   error: null,
 };
 
@@ -24,10 +32,34 @@ export const getPreOrders = createAsyncThunk(
   }
 );
 
+export const deletePreOrder = createAsyncThunk(
+  "delete-preorder",
+  async (details: DeletePreOrderType, thunkAPI) => {
+    const { preOrderId, setOpenDeletePreOrder } = details;
+    try {
+      const { status } = await axios.delete(
+        `/api/wishlist/admin/${preOrderId}`
+      );
+
+      if (status === 200) {
+        setOpenDeletePreOrder(false);
+      }
+
+      return preOrderId;
+    } catch (error) {
+      return thunkAPI.rejectWithValue("Something went wrong");
+    }
+  }
+);
+
 const preorderSlice = createSlice({
   name: "pre-order",
   initialState,
-  reducers: {},
+  reducers: {
+    setCurrentPreOrder: (state, action: PayloadAction<ProductTypes>) => {
+      state.singlePreOrder = action.payload;
+    },
+  },
   extraReducers(builder) {
     builder
       .addCase(getPreOrders.pending, (state) => {
@@ -44,7 +76,30 @@ const preorderSlice = createSlice({
           state.error = action.payload;
         }
       });
+    builder
+      .addCase(deletePreOrder.pending, (state) => {
+        state.loadingPreOrderAction = true;
+      })
+      .addCase(deletePreOrder.fulfilled, (state, action) => {
+        state.loadingPreOrderAction = false;
+        state.preOrders = state.preOrders.filter(
+          (preorder) => preorder._id !== action.payload
+        );
+        state.error = null;
+
+        toast.success("Pre-order successfully deleted", {
+          position: "top-center",
+          hideProgressBar: true,
+        });
+      })
+      .addCase(deletePreOrder.rejected, (state, action) => {
+        state.loadingPreOrderAction = false;
+        if (typeof action.payload === "string" || action.payload === null) {
+          state.error = action.payload;
+        }
+      });
   },
 });
 
+export const { setCurrentPreOrder } = preorderSlice.actions;
 export default preorderSlice.reducer;
