b625ffa110e5c1943735fb4b74110ddbc3de350c
Hamed Jimoh <hameed.jimoh1601@gmail.com>
2023-04-14 12:51:03 +0100

    Create DeletePreOrder component

---
+++ src/pages/pre-orders/modals/DeletePreOrder.tsx
@@ -0,0 +1,56 @@
+import React from "react";
+import ActionModal from "src/utils/ActionModal";
+import { useActions } from "src/hooks/useActions";
+import { useTypedSelector } from "src/hooks/useTypedSelector";
+
+type DeletePreOrderProps = {
+  openDeletePreOrder: boolean;
+  setOpenDeletePreOrder: React.Dispatch<React.SetStateAction<boolean>>;
+  actionType: string;
+  clickAction: () => void;
+  productName: string;
+  isClickAction: boolean;
+};
+
+const DeletePreOrder = (props: DeletePreOrderProps) => {
+  const {
+    openDeletePreOrder,
+    setOpenDeletePreOrder,
+    actionType,
+    clickAction,
+    productName,
+    isClickAction,
+  } = props;
+
+  const singlePreOrder = useTypedSelector(
+    (state) => state.preOrders.singlePreOrder
+  );
+  const loadingPreOrderAction = useTypedSelector(
+    (state) => state.preOrders.loadingPreOrderAction
+  );
+
+  const { deletePreOrder } = useActions();
+
+  const handleDeletePreOrder = () => {
+    deletePreOrder({
+      preOrderId: singlePreOrder?._id!,
+      setOpenDeletePreOrder,
+    });
+  };
+  return (
+    <ActionModal
+      actionType={actionType}
+      loading={loadingPreOrderAction}
+      openAction={openDeletePreOrder}
+      setOpenAction={setOpenDeletePreOrder}
+      data={productName}
+      handleAction={isClickAction ? clickAction : handleDeletePreOrder}
+    />
+  );
+};
+
+DeletePreOrder.defaultProps = {
+  DeletePreOrder: "Delete",
+};
+
+export default DeletePreOrder;
