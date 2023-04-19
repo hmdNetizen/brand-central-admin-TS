543f572fda82fe95f8dd7153ec8cd332b23a96e4
Hamed Jimoh <hameed.jimoh1601@gmail.com>
2023-04-19 18:41:02 +0100

    Implements OrderDetailsCard component

---
+++ src/components/orders/OrderDetailsCard.tsx
@@ -0,0 +1,128 @@
+import React from "react";
+import OrdersCardItem from "./OrderDetailsCardItem";
+import { useTheme } from "@mui/material/styles";
+import Chip from "@mui/material/Chip";
+import Grid from "@mui/material/Grid";
+import Typography from "@mui/material/Typography";
+import { capitalizeFirstLetters } from "src/lib/helpers";
+import Moment from "react-moment";
+import { OrderReturnedPayload } from "src/services/orders/OrderTypes";
+
+type OrderDetailsCardProps = {
+  singleOrder: OrderReturnedPayload;
+};
+
+const OrderDetailsCard = (props: OrderDetailsCardProps) => {
+  const { singleOrder } = props;
+  const theme = useTheme();
+
+  if (singleOrder) {
+    const {
+      orderId,
+      ordersProducts,
+      orderPaymentMethod,
+      orderStatus,
+      orderPaymentAmount,
+      orderDate,
+      orderPaymentStatus,
+      paymentReceipt,
+    } = singleOrder;
+    return (
+      <OrdersCardItem heading="Order Details" hasButton>
+        <tr>
+          <td>Order Number</td>
+          <td>{orderId}</td>
+        </tr>
+        <tr>
+          <td>Total Product</td>
+          <td>{ordersProducts.length}</td>
+        </tr>
+        <tr>
+          <td>Total Cost</td>
+          <td>${orderPaymentAmount.toFixed(2)}</td>
+        </tr>
+        <tr>
+          <td>Order Date</td>
+          <td>
+            <Moment format="D MMM YYYY" withTitle>
+              {orderDate}
+            </Moment>
+          </td>
+        </tr>
+        <tr>
+          <td>Payment Method</td>
+          <td>
+            {orderPaymentMethod === "Cash/Check"
+              ? "Cash on Delivery"
+              : orderPaymentMethod}
+          </td>
+        </tr>
+        <tr>
+          <td>Payment Status</td>
+          <td>
+            <Chip
+              label={capitalizeFirstLetters(orderPaymentStatus)}
+              style={{
+                fontSize: "1.25rem",
+                fontWeight: 500,
+                background:
+                  orderPaymentStatus === "unpaid"
+                    ? theme.palette.error.light
+                    : orderPaymentStatus === "paid"
+                    ? theme.palette.success.light
+                    : theme.palette.common.lightGreen,
+                color:
+                  orderPaymentStatus === "processing"
+                    ? theme.palette.success.dark
+                    : "#fff",
+              }}
+            />
+          </td>
+        </tr>
+        {orderPaymentMethod !== "Cash/Check" && (
+          <tr>
+            <td>Transaction ID</td>
+            <td>{paymentReceipt?.transactionDetails?.transactionId}</td>
+          </tr>
+        )}
+        <tr>
+          <td>Order Status</td>
+          <td>
+            <Chip
+              label={capitalizeFirstLetters(orderStatus)}
+              style={{
+                fontSize: "1.25rem",
+                background:
+                  orderStatus === "pending"
+                    ? theme.palette.warning.light
+                    : orderStatus === "declined"
+                    ? theme.palette.error.light
+                    : orderStatus === "completed"
+                    ? theme.palette.success.light
+                    : theme.palette.common.lightGreen,
+                color:
+                  orderStatus === "processing"
+                    ? theme.palette.success.dark
+                    : "#fff",
+              }}
+            />
+          </td>
+        </tr>
+      </OrdersCardItem>
+    );
+  } else {
+    return (
+      <Grid
+        item
+        container
+        justifyContent="center"
+        alignItems="center"
+        style={{ height: "70vh" }}
+      >
+        <Typography variant="h2">Order Not Found</Typography>
+      </Grid>
+    );
+  }
+};
+
+export default OrderDetailsCard;
--- src/components/orders/OrderDetailsCardItem.tsx
+++ src/components/orders/OrderDetailsCardItem.tsx
@@ -1,64 +1,16 @@
 import React from "react";
 import Grid from "@mui/material/Grid";
 import Typography from "@mui/material/Typography";
-import CustomIconButton from "src/utils/CustomIconButton";
 import VisibilitySharpIcon from "@mui/icons-material/VisibilitySharp";
-import { styled, useTheme } from "@mui/material/styles";
-import { Link, useParams, LinkProps } from "react-router-dom";
-
-const CardContainer = styled(Grid)({
-  background: "#fff",
-  paddingBottom: "1rem",
-  boxShadow: "-1px 0px 10px -2px rgba(0,0,0,0.15)",
-});
-
-const CardHeadingContainer = styled(Grid)({
-  padding: "1rem 2rem",
-  background: "#e2dbfc",
-});
-
-const CardTableWrapper = styled(Grid)({
-  padding: "1rem",
-});
-
-const Table = styled("table")(({ theme }) => ({
-  width: "100%",
-  borderRadius: 5,
-  "&, td": {
-    borderCollapse: "collapse",
-    border: `1px solid ${theme.palette.common.lighterGrey}`,
-  },
-
-  "& td": {
-    padding: "1rem 2rem",
-    fontSize: "1.5rem",
-    fontFamily: "Open Sans, Roboto",
-
-    [theme.breakpoints.only("xs")]: {
-      padding: ".5rem",
-    },
-  },
-
-  "& td:nth-of-type(1)": {
-    background: "rgba(249, 249, 249, 0.5)",
-    maxWidth: 100,
-  },
-
-  "& td:nth-of-type(2)": {
-    [theme.breakpoints.only("xs")]: {
-      minWidth: 0,
-    },
-  },
-}));
-
-const ActionButton = styled(CustomIconButton)<{
-  component: React.ElementType;
-  to: LinkProps["to"];
-}>({
-  minWidth: 64,
-  padding: "1rem 1.5rem",
-  borderRadius: "2rem",
-});
+import { useTheme } from "@mui/material/styles";
+import { Link, useParams } from "react-router-dom";
+import {
+  CardContainer,
+  CardHeadingContainer,
+  ActionButton,
+  CardTableWrapper,
+  Table,
+} from "./styles/OrderDetailsCardItemStyles";
 
 type OrderCardItemProps = {
   heading: string;
