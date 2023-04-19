9da371d26222a9b730733f9532945f398ef24685
Hamed Jimoh <hameed.jimoh1601@gmail.com>
2023-04-19 22:44:46 +0100

    Implements orderDetails page component

--- src/pages/orders/OrderDetails.tsx
+++ src/pages/orders/OrderDetails.tsx
@@ -6,39 +6,35 @@ import PreviousButton from "src/utils/PreviousButton";
 import KeyboardDoubleArrowRightSharpIcon from "@mui/icons-material/KeyboardDoubleArrowRightSharp";
 import { Link, useParams } from "react-router-dom";
 import OrderDetailsCard from "src/components/orders/OrderDetailsCard";
-import BillingDetailsCard from "components/orders/BillingDetailsCard";
-import ShippingDetailsCard from "components/orders/ShippingDetailsCard";
+import BillingDetailsCard from "src/components/orders/BillingDetailsCard";
+import ShippingDetailsCard from "src/components/orders/ShippingDetailsCard";
 import useMediaQuery from "@mui/material/useMediaQuery";
-import ProductOrderedList from "components/orders/ProductOrderedList";
+import OrderedProductsList from "src/components/orders/OrderedProductsList";
 import { useActions } from "src/hooks/useActions";
-import { useSelector } from "react-redux";
 import Spinner from "src/utils/Spinner";
 import useTitle from "src/hooks/useTitle";
+import { useTypedSelector } from "src/hooks/useTypedSelector";
 
-const useStyles = makeStyles((theme) => ({
-  container: {
-    "&.MuiGrid-root": {
-      padding: "1rem 2rem 5rem 2rem",
+const Container = styled(Grid)(({ theme }) => ({
+  padding: "1rem 2rem 5rem 2rem",
 
-      [theme.breakpoints.only("xs")]: {
-        padding: "5rem 1rem 5rem 1rem",
-      },
-    },
-  },
-  cardsContainer: {
-    background: "#fff",
-    padding: "2rem",
-    borderRadius: 5,
-
-    [theme.breakpoints.only("xs")]: {
-      padding: "2rem 1rem",
-    },
+  [theme.breakpoints.only("xs")]: {
+    padding: "5rem 1rem 5rem 1rem",
   },
 }));
 
-const OrderDetails = ({ menuSlideIn }) => {
+const CardsContainer = styled(Grid)(({ theme }) => ({
+  background: "#fff",
+  padding: "2rem",
+  borderRadius: 5,
+
+  [theme.breakpoints.only("xs")]: {
+    padding: "2rem 1rem",
+  },
+}));
+
+const OrderDetails = ({ menuSlideIn }: { menuSlideIn: boolean }) => {
   useTitle("Admin : Orders | Order details");
-  const classes = useStyles();
   const theme = useTheme();
 
   const { orderId } = useParams();
@@ -46,27 +42,27 @@ const OrderDetails = ({ menuSlideIn }) => {
   // const matchesMD = useMediaQuery(theme.breakpoints.only("md"));
   const matchesSM = useMediaQuery(theme.breakpoints.down("md"));
 
-  const { loadingSingleOrder, singleOrder } = useSelector(
-    (state) => state.orders
+  const loadingSingleOrder = useTypedSelector(
+    (state) => state.orders.loadingSingleOrder
   );
+  const singleOrder = useTypedSelector((state) => state.orders.singleOrder);
+
   const { getSingleOrder } = useActions();
 
   useLayoutEffect(() => {
-    getSingleOrder(orderId);
+    getSingleOrder(orderId!);
 
     // eslint-disable-next-line
   }, [orderId]);
 
   return (
-    <Grid container direction="column" className={classes.container}>
+    <Container container direction="column">
       <Grid item container alignItems="center" columnSpacing={1}>
         <Grid item>
           <PreviousButton path="orders" />
         </Grid>
         <Grid item>
-          <Typography variant="h2" className={classes.pageHeading}>
-            Order Details
-          </Typography>
+          <Typography variant="h2">Order Details</Typography>
         </Grid>
       </Grid>
       <Grid
@@ -105,12 +101,7 @@ const OrderDetails = ({ menuSlideIn }) => {
           <Spinner loaderHeight="100%" />
         </Grid>
       ) : !loadingSingleOrder && singleOrder ? (
-        <Grid
-          item
-          container
-          direction="column"
-          className={classes.cardsContainer}
-        >
+        <CardsContainer item container direction="column">
           <Grid
             item
             container
@@ -119,23 +110,26 @@ const OrderDetails = ({ menuSlideIn }) => {
             //   direction={matchesSM ? "column" : "row"}
           >
             <Grid item xs={12} md={!menuSlideIn ? 12 : 6} lg={6}>
-              <OrderDetailsCard />
+              <OrderDetailsCard singleOrder={singleOrder} />
             </Grid>
             <Grid item xs={12} md={!menuSlideIn ? 12 : 6} lg>
-              <BillingDetailsCard />
+              <BillingDetailsCard singleOrder={singleOrder} />
             </Grid>
           </Grid>
 
           <Grid item container spacing={3} sx={{ pt: 3 }}>
             <Grid item xs={12} md={!menuSlideIn ? 12 : 6} lg={6}>
-              <ShippingDetailsCard />
+              <ShippingDetailsCard singleOrder={singleOrder} />
             </Grid>
             <Grid item xs={12} md={!menuSlideIn ? 12 : 6} lg={6}></Grid>
           </Grid>
           <Grid item container>
-            <ProductOrderedList />
+            <OrderedProductsList
+              loading={loadingSingleOrder}
+              singleOrder={singleOrder}
+            />
           </Grid>
-        </Grid>
+        </CardsContainer>
       ) : (
         <Grid
           item
@@ -147,7 +141,7 @@ const OrderDetails = ({ menuSlideIn }) => {
           <Typography variant="h2">No Order found</Typography>
         </Grid>
       )}
-    </Grid>
+    </Container>
   );
 };
 
