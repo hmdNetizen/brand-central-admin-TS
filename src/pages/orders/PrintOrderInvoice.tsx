dfb4f782558cd41f58f9c2ba122bd0205326ea20
Hamed Jimoh <hameed.jimoh1601@gmail.com>
2023-04-21 17:14:16 +0100

    Completes the implementation of the printOrderInvoice component

--- src/pages/orders/PrintOrderInvoice.tsx
+++ src/pages/orders/PrintOrderInvoice.tsx
@@ -1,7 +1,25 @@
-import React from "react";
+import { useEffect } from "react";
 import OrderInvoicePageDisplay from "./OrderInvoicePageDisplay";
+import { useTypedSelector } from "src/hooks/useTypedSelector";
 
 const PrintOrderInvoice = () => {
+  const loadingSingleOrder = useTypedSelector(
+    (state) => state.orders.loadingSingleOrder
+  );
+
+  useEffect(() => {
+    let timer: NodeJS.Timeout;
+
+    if (!loadingSingleOrder) {
+      timer = setTimeout(() => {
+        window.print();
+      }, 3000);
+    }
+
+    return () => clearTimeout(timer);
+    // eslint-disable-next-line
+  }, []);
+
   return <OrderInvoicePageDisplay />;
 };
 
