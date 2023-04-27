46c9b0ca4ab7af59804e88909ec879278a281914
Hamed Jimoh <hameed.jimoh1601@gmail.com>
2023-04-27 22:10:09 +0100

    Adds BrandsCategoryItem component

---
+++ src/components/brands-categories/BrandsCategoryItem.tsx
@@ -0,0 +1,85 @@
+import React from "react";
+import TableRow from "@mui/material/TableRow";
+import TableCell from "@mui/material/TableCell";
+import DeleteSharpIcon from "@mui/icons-material/DeleteSharp";
+import EditSharpIcon from "@mui/icons-material/EditSharp";
+import CustomSwitch from "src/utils/CustomSwitch";
+import { useTheme } from "@mui/material/styles";
+import {
+  OptionsTableData,
+  ActionButton,
+  StyledIconButton,
+} from "src/components/common/styles/CommonPageStyles";
+import { useActions } from "src/hooks/useActions";
+import { BrandsCategoryReturnedPayload } from "src/services/categories/CategoryTypes";
+
+type BrandsCategoryProps = {
+  brandCategory: BrandsCategoryReturnedPayload;
+  setOpenEditBrandCategory: React.Dispatch<React.SetStateAction<boolean>>;
+  setOpenDeleteBrandCategory: React.Dispatch<React.SetStateAction<boolean>>;
+};
+
+const BrandsCategoryItem = (props: BrandsCategoryProps) => {
+  const theme = useTheme();
+  const {
+    brandCategory,
+    setOpenDeleteBrandCategory,
+    setOpenEditBrandCategory,
+  } = props;
+  const { category, categorySlug, isActivate, name, subCategory } =
+    brandCategory;
+  const { setCurrentBrandCategory, toggleBrandCategoryActivation } =
+    useActions();
+
+  const handleSwitchChange = () => {
+    toggleBrandCategoryActivation({
+      brandCategoryId: brandCategory._id,
+      isActivate: !brandCategory.isActivate,
+    });
+  };
+
+  const handleEditBrandCategory = () => {
+    setOpenEditBrandCategory(true);
+    setCurrentBrandCategory(brandCategory);
+  };
+
+  const handleDeleteSubCategory = () => {
+    setOpenDeleteBrandCategory(true);
+    setCurrentBrandCategory(brandCategory);
+  };
+
+  return (
+    <TableRow hover role="checkbox" tabIndex={-1}>
+      <TableCell sx={{ minWidth: 200 }}>{category.toUpperCase()}</TableCell>
+      <TableCell sx={{ minWidth: 150 }}>{subCategory.toUpperCase()}</TableCell>
+      <TableCell sx={{ minWidth: 150 }}>{name.toUpperCase()}</TableCell>
+      <TableCell sx={{ minWidth: 150 }} align="center">
+        {categorySlug.toLowerCase()}
+      </TableCell>
+      <TableCell align="center">
+        <CustomSwitch
+          color={isActivate ? "success" : "error"}
+          onChange={handleSwitchChange}
+          checked={isActivate}
+          isActive={isActivate}
+        />
+      </TableCell>
+      <TableCell>
+        <OptionsTableData>
+          <ActionButton
+            startIcon={<EditSharpIcon />}
+            background={theme.palette.secondary}
+            title="Edit"
+            onClick={handleEditBrandCategory}
+          />
+
+          <StyledIconButton onClick={handleDeleteSubCategory}>
+            <DeleteSharpIcon />
+          </StyledIconButton>
+        </OptionsTableData>
+      </TableCell>
+    </TableRow>
+  );
+};
+
+export default BrandsCategoryItem;
