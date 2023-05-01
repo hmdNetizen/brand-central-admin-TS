cc667f0c06cb289f05e7b7636f77e8930b758ee9
Hamed Jimoh <hameed.jimoh1601@gmail.com>
2023-05-01 15:48:25 +0100

    Create the DeactivatedBrands page

---
+++ src/pages/brands/DeactivatedBrands.tsx
@@ -0,0 +1,56 @@
+import { useState, useEffect } from "react";
+import { useActions } from "src/hooks/useActions";
+import useTitle from "src/hooks/useTitle";
+import { useTypedSelector } from "src/hooks/useTypedSelector";
+import BrandPageLayout from "./BrandPageLayout";
+
+const DeactivatedBrands = () => {
+  useTitle("Admin : List of all Deactivated Brands");
+
+  const [page, setPage] = useState(0);
+  const [rowsPerPage, setRowsPerPage] = useState(10);
+  const [filterText, setFilterText] = useState("");
+  const [openAddBrand, setOpenAddBrand] = useState(false);
+  const [openEditBrand, setOpenEditBrand] = useState(false);
+  const [openDeleteBrand, setOpenDeleteBrand] = useState(false);
+
+  const brands = useTypedSelector((state) => state.brands.brands);
+
+  const { getAllBrands, getSearchedBrands } = useActions();
+
+  useEffect(() => {
+    if (!filterText) {
+      getAllBrands({
+        page: page + 1,
+        limit: rowsPerPage,
+      });
+    } else {
+      getSearchedBrands({
+        page: page + 1,
+        limit: rowsPerPage,
+        searchTerm: filterText,
+      });
+    }
+  }, [filterText, page, rowsPerPage]);
+
+  return (
+    <BrandPageLayout
+      brands={brands.filter((brand) => brand.isActivated)}
+      filterText={filterText}
+      page={page}
+      rowsPerPage={rowsPerPage}
+      setFilterText={setFilterText}
+      setOpenAddBrand={setOpenAddBrand}
+      setOpenDeleteBrand={setOpenDeleteBrand}
+      setOpenEditBrand={setOpenEditBrand}
+      setPage={setPage}
+      setRowsPerPage={setRowsPerPage}
+      openAddBrand={openAddBrand}
+      openEditBrand={openEditBrand}
+      openDeleteBrand={openDeleteBrand}
+      isDeactivatedPage={true}
+    />
+  );
+};
+
+export default DeactivatedBrands;
