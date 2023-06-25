5fde7c0c29e60c2cfba3ba9e3327b43acc993c5a
Hamed Jimoh <hameed.jimoh1601@gmail.com>
2023-06-25 21:07:30 +0100

    Adds onSubmit event listener as a prop

---
+++ src/pages/products/utils/ProductHighlightForm.tsx
@@ -0,0 +1,178 @@
+import React, { ElementType } from "react";
+import Grid from "@mui/material/Grid";
+import { styled } from "@mui/material/styles";
+import {
+  SubmitButton,
+  CancelButton,
+  StyledCircularProgress,
+} from "src/utilityStyles/pagesUtilityStyles";
+import CustomLabelSwitch from "src/utils/CustomLabelSwitch";
+import CustomFormInput from "src/utils/CustomFormInput";
+
+const HighlightContainer = styled(Grid)<{ component: ElementType }>(
+  ({ theme }) => ({
+    padding: "1rem 5rem 2rem",
+
+    [theme.breakpoints.down("md")]: {
+      padding: "1rem 2rem 2rem",
+    },
+    [theme.breakpoints.only("xs")]: {
+      padding: "1rem 1rem 2rem",
+    },
+  })
+);
+
+type HighlightFormProps = {
+  onSubmit: (event: React.FormEvent<Element>) => void;
+};
+
+const ProductHighlightForm = (props: HighlightFormProps) => {
+  const { onSubmit } = props;
+  return (
+    <HighlightContainer
+      item
+      container
+      direction="column"
+      component="form"
+      onSubmit={onSubmit}
+    >
+      <Grid item style={{ marginBottom: "2rem" }}>
+        <CustomLabelSwitch
+          label="Highlight in Featured"
+          name="inFeatured"
+          onChange={handleHighlightChecked}
+          checked={inFeatured}
+          isActive={inFeatured}
+        />
+      </Grid>
+      <Grid item style={{ marginBottom: "2rem" }}>
+        <CustomLabelSwitch
+          label="Highlight in Best Sellers"
+          name="inBestSellers"
+          checked={inBestSellers}
+          isActive={inBestSellers}
+          onChange={handleHighlightChecked}
+        />
+      </Grid>
+      <Grid item style={{ marginBottom: "2rem" }}>
+        <CustomLabelSwitch
+          label="Highlight in Popular"
+          name="inPopular"
+          checked={inPopular}
+          isActive={inPopular}
+          onChange={handleHighlightChecked}
+        />
+      </Grid>
+      <Grid item container direction="column">
+        <Grid item style={{ marginBottom: "2rem" }}>
+          <CustomLabelSwitch
+            label="Highlight in Weekly Offers"
+            name="inWeeklyOffer"
+            checked={inWeeklyOffer}
+            isActive={inWeeklyOffer}
+            onChange={handleHighlightChecked}
+          />
+        </Grid>
+        {inWeeklyOffer && (
+          <Grid item container direction="column">
+            <Grid item container columnGap={3} rowSpacing={2} sx={{ mb: 2 }}>
+              <Grid item sm xs={12}>
+                <CustomFormInput
+                  type="number"
+                  label="Price Code 1"
+                  labelId="priceCode1"
+                  name="priceCode1"
+                  value={priceCode1}
+                  placeholder="Enter Price Code 1"
+                  onChange={handleInputChange}
+                  error={priceCode1Error}
+                />
+              </Grid>
+              <Grid item sm xs={12}>
+                <CustomFormInput
+                  type="number"
+                  label="Price Code 2"
+                  labelId="priceCode2"
+                  name="priceCode2"
+                  value={priceCode2}
+                  placeholder="Enter Price Code 2"
+                  onChange={handleInputChange}
+                  error={priceCode2Error}
+                />
+              </Grid>
+            </Grid>
+            <Grid item container columnGap={3} rowSpacing={2} mb={2}>
+              <Grid item sm xs={12}>
+                <CustomFormInput
+                  type="number"
+                  label="Price Code 3"
+                  labelId="priceCode3"
+                  name="priceCode3"
+                  value={priceCode3}
+                  placeholder="Enter Price Code 3"
+                  onChange={handleInputChange}
+                  error={priceCode3Error}
+                />
+              </Grid>
+              <Grid item sm xs={12}>
+                <CustomFormInput
+                  type="number"
+                  label="Price Code 4"
+                  labelId="priceCode4"
+                  name="priceCode4"
+                  value={priceCode4}
+                  placeholder="Enter Price Code 4"
+                  onChange={handleInputChange}
+                  error={priceCode4Error}
+                />
+              </Grid>
+            </Grid>
+            <Grid item container columnGap={3} rowSpacing={2}>
+              <Grid item sm xs={12}>
+                <CustomFormInput
+                  type="number"
+                  label="Minimum Quantity"
+                  labelId="minimumQuantity"
+                  name="minimumQuantity"
+                  value={minimumQuantity}
+                  placeholder="Enter Minmimum Quantity"
+                  onChange={handleInputChange}
+                  error={minQuantityError}
+                />
+              </Grid>
+              <Grid item sm xs={12}></Grid>
+            </Grid>
+          </Grid>
+        )}
+      </Grid>
+      <Grid
+        item
+        container
+        alignItems="center"
+        justifyContent="center"
+        columnSpacing={1}
+        style={{ marginTop: "5rem" }}
+      >
+        <Grid item>
+          <CancelButton onClick={handleClose}>Cancel</CancelButton>
+        </Grid>
+        <Grid item>
+          <SubmitButton
+            type="submit"
+            variant="contained"
+            disableRipple
+            color="secondary"
+            disabled={loadingProductAction}
+          >
+            {loadingProductAction && (
+              <StyledCircularProgress style={{ height: 25, width: 25 }} />
+            )}{" "}
+            Submit
+          </SubmitButton>
+        </Grid>
+      </Grid>
+    </HighlightContainer>
+  );
+};
+
+export default ProductHighlightForm;
