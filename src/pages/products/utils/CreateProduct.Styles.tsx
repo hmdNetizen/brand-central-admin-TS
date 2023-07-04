925a63563e7980627cd52d7c577710d39f333443
Hamed Jimoh <hameed.jimoh1601@gmail.com>
2023-07-04 14:37:00 +0100

    Moves the StyledButton to the CreateProductStyles file

--- src/pages/products/utils/CreateProduct.Styles.tsx
+++ src/pages/products/utils/CreateProduct.Styles.tsx
@@ -1,6 +1,7 @@
 import { styled } from "@mui/material/styles";
 import Grid from "@mui/material/Grid";
 import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
 
 export const Content = styled(Grid)({
   boxShadow: "-1px 0px 10px -2px rgba(0,0,0,0.15)",
@@ -19,4 +20,20 @@ export const SuccessMessage = styled(Typography)(({ theme }) => ({
   color: theme.palette.success.main,
   fontWeight: 400,
   fontSize: "1.35rem",
+}));
+
+export const StyledButton = styled(Button)(({ theme }) => ({
+  padding: "1rem",
+  fontSize: "1.5rem",
+  textTransform: "none",
+  background: theme.palette.primary.light,
+  borderRadius: 0,
+
+  "&:hover": {
+    background: theme.palette.primary.light,
+  },
+
+  "&:active": {
+    background: theme.palette.primary.main,
+  },
 }));
