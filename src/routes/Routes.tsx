4156146ed58af2fc2ae40146451d9b3a39aa8cd8
Hamed Jimoh <hameed.jimoh1601@gmail.com>
2023-07-10 11:33:00 +0100

    Adds PopularProducts component to the products route

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -29,6 +29,7 @@ import PreOrders from "src/pages/pre-orders/PreOrders";
 import AllProducts from "src/pages/products/AllProducts";
 import CreateProduct from "src/pages/products/CreateProduct";
 import FeaturedProducts from "src/pages/products/FeaturedProducts";
+import PopularProducts from "src/pages/products/Popular";
 import WeeklyOffersProducts from "src/pages/products/WeeklyOffers";
 import ShippingZipCodes from "src/pages/shipping/ShippingZipCodes";
 import SubCategories from "src/pages/subcategories/SubCategories";
@@ -77,6 +78,7 @@ const PagesRoutes = () => {
                 <Route index element={<AllProducts />} />
                 <Route path="create" element={<CreateProduct />} />
                 <Route path="featured" element={<FeaturedProducts />} />
+                <Route path="popular" element={<PopularProducts />} />
                 <Route
                   path="weekly-offers"
                   element={<WeeklyOffersProducts />}
