3517af7a387bd531df661bc8e7abb02ee87b3919
Hamed Jimoh <hameed.jimoh1601@gmail.com>
2023-07-11 18:43:11 +0100

    Implements the Notification component

---
+++ src/components/notifications/Notifications.tsx
@@ -0,0 +1,76 @@
+import React, { Fragment, useEffect } from "react";
+import NotificationItem from "./NotificationItem";
+import Typography from "@mui/material/Typography";
+import IconButton from "@mui/material/IconButton";
+import useMediaQuery from "@mui/material/useMediaQuery";
+import CloseIcon from "@mui/icons-material/Close";
+import { useTheme } from "@mui/material/styles";
+import NotificationItemMobile from "./NotificationItemMobile";
+import { useTypedSelector } from "src/hooks/useTypedSelector";
+import { HeadingWrapper } from "./styles";
+
+type NotificationProps = {
+  setShowNotification: React.Dispatch<React.SetStateAction<boolean>>;
+  productCode: string;
+  setProductCode: React.Dispatch<React.SetStateAction<string>>;
+};
+
+const Notification = (props: NotificationProps) => {
+  const theme = useTheme();
+  const { setShowNotification, productCode, setProductCode } = props;
+
+  const matchesSM = useMediaQuery(theme.breakpoints.down("md"));
+
+  const loadingPreOrderAction = useTypedSelector(
+    (state) => state.preOrders.loadingPreOrderAction
+  );
+  const preOrdersUpdatedStock = useTypedSelector(
+    (state) => state.preOrders.preOrdersUpdatedStock
+  );
+
+  const handleCloseButton = () => {
+    sessionStorage.setItem("hidePreOrderNotification", JSON.stringify(true));
+
+    setShowNotification(false);
+  };
+
+  useEffect(() => {
+    if (!loadingPreOrderAction) {
+      setProductCode("");
+    }
+
+    // eslint-disable-next-line
+  }, [loadingPreOrderAction]);
+
+  return (
+    <Fragment>
+      <HeadingWrapper item container justifyContent="space-between">
+        <Typography variant="body1" color="#fff">
+          Available Stock Notification
+        </Typography>
+        <IconButton onClick={handleCloseButton}>
+          <CloseIcon style={{ color: "#fff" }} />
+        </IconButton>
+      </HeadingWrapper>
+      {preOrdersUpdatedStock.map((stock) => (
+        <Fragment key={stock.id}>
+          {matchesSM ? (
+            <NotificationItemMobile
+              stock={stock}
+              productCode={productCode}
+              setProductCode={setProductCode}
+            />
+          ) : (
+            <NotificationItem
+              stock={stock}
+              productCode={productCode}
+              setProductCode={setProductCode}
+            />
+          )}
+        </Fragment>
+      ))}
+    </Fragment>
+  );
+};
+
+export default Notification;
