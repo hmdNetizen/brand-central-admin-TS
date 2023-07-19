9f2aecb479da5ca98f5cc4867f9462629494b8d9
Hamed Jimoh <hameed.jimoh1601@gmail.com>
2023-07-19 12:51:40 +0100

    Creates a SalesPersonTypes in the services directory

---
+++ src/services/salespersons/SalesPersonTypes.ts
@@ -0,0 +1,9 @@
+export type SalesPersonReturnedPayload = {
+  _id: string;
+  fullName: string;
+  email: string;
+  initials: string;
+  phoneNumber: string;
+  profileImage: string;
+  customers: [];
+};
