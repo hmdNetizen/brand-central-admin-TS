9f8bdef08b5ee9aa3c3c56ca000800a0f52d1dd2
Hamed Jimoh <hameed.jimoh1601@gmail.com>
2023-10-10 18:40:52 +0100

    Adds a url for development

--- src/services/BASE_URL.ts
+++ src/services/BASE_URL.ts
@@ -1,3 +1,4 @@
 export const BASE_URL = import.meta.env.VITE_BASE_URL;
+export const DEV_BASE_URL = import.meta.env.VITE_DEV_BASE_URL;
 // export const BASE_URL = "http://localhost:4000";
 // export const BASE_URL = "http://localhost:4000";
--- src/services/axios.ts
+++ src/services/axios.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
-import { BASE_URL } from "src/services/BASE_URL";
+import { BASE_URL, DEV_BASE_URL } from "src/services/BASE_URL";
 
 export default axios.create({
-  baseURL: BASE_URL,
+  baseURL: DEV_BASE_URL,
   headers: {
     "Content-Type": "application/json",
   },
