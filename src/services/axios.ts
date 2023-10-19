2dcf3da093789b6eab93b8c1d16f4e5cdbfbd4c8
Hamed Jimoh <hameed.jimoh1601@gmail.com>
2023-10-19 17:23:00 +0100

    Changes the baseurl from the test mode to the live mode

--- src/services/axios.ts
+++ src/services/axios.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import { BASE_URL, DEV_BASE_URL } from "src/services/BASE_URL";
 
 export default axios.create({
-  baseURL: DEV_BASE_URL,
+  baseURL: BASE_URL,
   headers: {
     "Content-Type": "application/json",
   },
