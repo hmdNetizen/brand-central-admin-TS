2a497d6347a392a4715524695899b59056e63bb5
Hamed Jimoh <hameed.jimoh1601@gmail.com>
2024-01-12 17:51:17 +0100

    Switches the base url to the live api url

--- src/services/axios.ts
+++ src/services/axios.ts
@@ -2,8 +2,8 @@ import axios from "axios";
 import { BASE_URL, DEV_BASE_URL } from "src/services/BASE_URL";
 
 export default axios.create({
-  baseURL: "http://localhost:4000",
-  // baseURL: DEV_BASE_URL,
+  // baseURL: "http://localhost:4000",
+  baseURL: DEV_BASE_URL,
   headers: {
     "Content-Type": "application/json",
   },
